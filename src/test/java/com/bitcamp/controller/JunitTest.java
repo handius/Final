77abaf6ce90e8620d677049a11f37679618bddcb
KyeongJin <dyttl0@gmail.com>
2019-08-05 12:38:36 +0900

    test

--- src/test/java/com/bitcamp/controller/JunitTest.java
+++ src/test/java/com/bitcamp/controller/JunitTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.bitcamp.DTO.MainViewDTO;
+import com.bitcamp.DTO.admin.MainViewDTO;
 import com.bitcamp.comm.DBConfigration;
 import com.bitcamp.mapper.TestMapper;
 
