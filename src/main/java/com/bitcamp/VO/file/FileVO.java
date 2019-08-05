43b1875849ef3ed58f8eea9fe4efecdceaecb8ed
hong <hong@naver.com>
2019-08-05 12:42:12 +0900

    VO package 추가함. OrderOptionDTO 추가함

---
+++ src/main/java/com/bitcamp/VO/file/FileVO.java
@@ -0,0 +1,16 @@
+package com.bitcamp.VO.file;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class FileVO {
+	private String fileName;
+	private MultipartFile file;
+}
