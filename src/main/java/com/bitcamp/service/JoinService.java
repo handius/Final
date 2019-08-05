fad62f31fedbc00582554b99af6179fd912af35a
yonzyun <vinyl1@naver.com>
2019-08-05 18:05:21 +0900

    context에 mapper scan 추가

--- src/main/java/com/bitcamp/service/JoinService.java
+++ src/main/java/com/bitcamp/service/JoinService.java
@@ -1,10 +1,16 @@
 package com.bitcamp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.bitcamp.mapper.JoinMapper;
 
 @Service("joinService")
 public class JoinService {
 
+	@Autowired
+	JoinMapper joinMapper;
+	
 	public int userIdCheck(String user_id) {
 		return 1;
 	}
