f520c8d82562231e05e885974360258ac3af69d5
jiseonmoon <mjs8192@naver.com>
2019-08-05 16:02:09 +0900

    DTO 추가

---
+++ src/main/java/com/bitcamp/DTO/customerqaboard/CustomerQABoardDTO.java
@@ -0,0 +1,13 @@
+package com.bitcamp.DTO.customerqaboard;
+
+public class CustomerQABoardDTO {
+	private int question_no;
+	private int member_no;
+	private String question_title;
+	private String question_content;
+	private String question_date;
+	private String answer_content;
+	private String answer_status;
+	private boolean question_delete_status;
+	private String question_type;
+}
