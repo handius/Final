85a3ffbfc0d1df1b1ddfb76fa799204b765d9a52
yonzyun <vinyl1@naver.com>
2019-08-05 17:57:55 +0900

    회원가입 폼 수정중

--- src/main/java/com/bitcamp/controller/JoinController.java
+++ src/main/java/com/bitcamp/controller/JoinController.java
@@ -1,18 +1,45 @@
 package com.bitcamp.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bitcamp.service.JoinService;
 
 @Controller
 public class JoinController {
+	
+	@Resource(name="joinService")
+	private JoinService joinService;
 
+	@RequestMapping("join/terms")
+	private String terms() {
+		return "join/terms";
+	}
+	
 	@RequestMapping("join/joinform")
 	public String joinform() {
 		return "join/joinform";
 	}
 	
-	@RequestMapping("join/terms")
-	private String terms() {
-		return "join/terms";
+	@RequestMapping("join/joinResult")
+	public String join(@RequestParam String id, @RequestParam String password1, @RequestParam String password2, @RequestParam String name, @RequestParam String email, @RequestParam String address1, @RequestParam String address2) {
+		System.out.println(id);
+		System.out.println(password1);
+		return "";
 	}
+	
+	@RequestMapping(value="/user/idCheck", method=RequestMethod.GET)
+	@ResponseBody
+	public int check(@RequestParam("userId") String user_id) {
+		System.out.println("dddddddddddd");
+		System.out.println(user_id);
+		return joinService.userIdCheck(user_id);
+	}
+	
 }
---
+++ src/main/java/com/bitcamp/service/JoinService.java
@@ -0,0 +1,12 @@
+package com.bitcamp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("joinService")
+public class JoinService {
+
+	public int userIdCheck(String user_id) {
+		return 1;
+	}
+
+}
