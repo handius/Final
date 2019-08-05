f8b07245b1c72108287fcaad831030b9e607d2a4
hong <hong@naver.com>
2019-08-05 12:30:14 +0900

    Service package 추가함. Product Controller 추가함

---
+++ src/main/java/com/bitcamp/DTO/Product/ListDTO.java
@@ -0,0 +1,30 @@
+package com.bitcamp.DTO.Product;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter @Setter
+public class ListDTO {
+	private int list_no;
+	private String list_title;
+	private String list_artist;
+	private String list_product;
+	private int list_base_price;
+	private String list_category;
+	private String list_content;
+	private String list_date;
+	private List<String> list_image_loc;
+	private List<String> list_hash;
+	private int isordered;
+	private List<String> option_name;
+	private List<Integer> option_stock;
+	private List<Integer> option_price;
+	private String ordered_content;
+	private List<String> order_name;
+	private List<String> order_option;
+	private int buy_count;
+}
---
+++ src/main/java/com/bitcamp/controller/ProductController.java
@@ -0,0 +1,8 @@
+package com.bitcamp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+	
+}
