c84c5a4f3db35df406f7b01a54cd8400ad5abcc7
KyeongJin <dyttl0@gmail.com>
2019-08-05 13:12:11 +0900

    틀 잡는 중

---
+++ src/main/java/com/bitcamp/VO/admin/DateVO.java
@@ -0,0 +1,11 @@
+package com.bitcamp.VO.admin;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class DateVO {
+	private int search_date_year;
+	private int search_date_month;
+	private int search_date_day;
+}
