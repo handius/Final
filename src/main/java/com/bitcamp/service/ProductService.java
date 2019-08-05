e198e4eb83a1d5dd8fd6fd64b3ebe9988e276b59
hong <hong@naver.com>
2019-08-05 13:17:27 +0900

    ProductService 메소드 추가합니다.

--- src/main/java/com/bitcamp/service/ProductService.java
+++ src/main/java/com/bitcamp/service/ProductService.java
@@ -1,8 +1,36 @@
 package com.bitcamp.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+import com.bitcamp.DTO.Product.ListDTO;
+import com.bitcamp.DTO.comm.PageDTO;
+import com.bitcamp.mapper.ProductListMapper;
+
+@Service("service")
 public class ProductService {
+	@Autowired
+	private ProductListMapper mapper;
 	
+	public List<ListDTO> getListService(String searchType, String searchData, PageDTO dto, String list_category, List<String> hashTag, Boolean hasStock, int status){
+		return null;
+	}
+	
+	public int getCountService(String searchType, String searchData, String list_category, List<String> hashTag, Boolean hasStock, int status) {
+		return 0;
+	}
+	
+	public int insertPerfectOrderDataService(ListDTO dto) {
+		return 0;
+	}
+	
+	public int insertOrderMadeDataService(ListDTO dto) {
+		return 0;
+	}
+	
+	public int insertOrderOptionService(List<Integer> list_order_no, List<String> order_value) {
+		return 0;
+	}
 }
