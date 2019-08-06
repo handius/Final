24ae96751d94aff2c0e6215a3678158867f088be
jiseonmoon <mjs8192@naver.com>
2019-08-06 18:32:57 +0900

    음

--- src/main/java/com/bitcamp/controller/OrderController.java
+++ src/main/java/com/bitcamp/controller/OrderController.java
@@ -1,27 +1,25 @@
 package com.bitcamp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bitcamp.service.OrderService;
 
 @Controller
 public class OrderController {
-	@RequestMapping("order/order")
-	public String a() {
+	@Autowired
+	private OrderService orderService;
+
+	@RequestMapping("order/order/{list_no}")
+	public String a(@PathVariable int list_no, Model model) {
 		return "order/order";
 	}
 
-	@RequestMapping("order/payment")
-	public String b() {
-		return "order/payment";
-	}
-
-	@RequestMapping("order/success")
-	public String c() {
-		return "order/success";
-	}
-
-	@RequestMapping("order/failure")
-	public String d() {
-		return "order/failure";
+	@RequestMapping("order/orderResult")
+	public String success() {
+		return "order/orderResult";
 	}
 }
