4a877d4b7772af736dcade9fbdddaf848619fb64
yonzyun <vinyl1@naver.com>
2019-08-08 16:27:53 +0900

    ROLE_MEMBER 권한 업데ㅇ;트

--- src/main/java/com/bitcamp/mapper/JoinMapper.java
+++ src/main/java/com/bitcamp/mapper/JoinMapper.java
@@ -1,5 +1,8 @@
 package com.bitcamp.mapper;
 
+import java.util.Map;
+
+import com.bitcamp.DTO.member.AuthorityDTO;
 import com.bitcamp.DTO.member.MemberDTO;
 
 public interface JoinMapper {
@@ -7,9 +10,18 @@ public interface JoinMapper {
 	public int userIdCheck(String user_id);
 	
 	public void userRegister(MemberDTO dto);
+	public void userAuthRegister(int member_no);
 
+	public int userSelect(String user_id);
+	
 	public int compareCertKey(MemberDTO dto);
 
 	public int updateCertStatus(String user_id);
+	public void updateMemberAuth(int member_no);
+
+
+
+
+
 	
 }
