2950b42be21ffae700fb6f396782f7c990270b53
yonzyun <vinyl1@naver.com>
2019-08-09 12:57:22 +0900

    회원정보 값 가져오기

--- src/main/java/com/bitcamp/controller/LoginContoller.java
+++ src/main/java/com/bitcamp/controller/LoginContoller.java
@@ -1,14 +1,19 @@
 package com.bitcamp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bitcamp.DTO.member.MemberDTO;
+import com.bitcamp.mapper.JoinMapper;
+import com.bitcamp.mapper.MemberMapper;
+
 @Controller
 public class LoginContoller {
-	
+
 	@RequestMapping("login")
 	public String login(String error, String logout, Model model) {
 		if(error!=null) {
@@ -17,6 +22,7 @@ public class LoginContoller {
 		if(logout!=null) {
 			model.addAttribute("logout","logout..");
 		}
+
 		return "login/loginform";
 	}
 	
@@ -44,8 +50,7 @@ public class LoginContoller {
 	@RequestMapping("login/loginResult")
 	public String loginResult() {
 	
-		
-		return "/main";
+		return "login/logintest";
 	}
 	
 	@RequestMapping("login/searchIDResult")
