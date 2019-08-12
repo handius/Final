bb0276d66487d19956c5700dcf4cfd64ea64bb13
jiseonmoon <mjs8192@naver.com>
2019-08-12 16:53:41 +0900

    order 일단 완성

--- src/main/java/com/bitcamp/service/OrderService.java
+++ src/main/java/com/bitcamp/service/OrderService.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.meta.TypeQualifier;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,40 +25,44 @@ public class OrderService {
 	public void buyProduct(OrderDTO orderDTO) {
 		List<Integer> order_add_option = orderDTO.getOrder_add_option();
 		List<Integer> order_amount = orderDTO.getOrder_amount();
-		String add_option = "";
-		String amount = "";
+		StringBuilder add_option = new StringBuilder();
+		StringBuilder amount = new StringBuilder();
 		List<Integer> ordermade_no = orderDTO.getOrdermade_no();
-		String made_no = "";
+		StringBuilder made_no = new StringBuilder();
 		for (int i = 0; i < order_add_option.size(); i++) {
-			add_option = add_option + order_add_option.get(i);
-			amount = amount + order_amount.get(i);
+			add_option.append(order_add_option.get(i) + "/");
+			amount.append(order_amount.get(i) + "/");
 			if (i < order_add_option.size() - 1) {
-				add_option = add_option + "/";
-				amount = amount + "/";
+				add_option.append("/");
+				amount.append("/");
 			}
 		}
 		for (int i = 0; i < ordermade_no.size(); i++) {
-			made_no = made_no + ordermade_no.get(i);
+			made_no.append(ordermade_no.get(i) + "/");
 			if (i < ordermade_no.size() - 1) {
-				made_no = made_no + "/";
+				made_no.append("/");
 			}
 		}
-		List<Map<Integer, Integer>> option = new ArrayList<>();
+
+		List<Map<String, Integer>> option = new ArrayList<>();
 		for (int i = 0; i < order_add_option.size(); i++) {
-			Map<Integer, Integer> noStock = new HashMap<>();
-			noStock.put(order_add_option.get(i), order_amount.get(i));
+			Map<String, Integer> noStock = new HashMap<>();
+			noStock.put("order_add_option", order_add_option.get(i));
+			noStock.put("order_amount", order_amount.get(i));
 			option.add(noStock);
 		}
+
 		Map<String, Object> parameters = new HashMap<>();
 		parameters.put("list_no", orderDTO.getList_no());
 		parameters.put("member_no", orderDTO.getMember_no());
 		parameters.put("order_price", orderDTO.getOrder_price());
-		parameters.put("order_add_option", add_option);
-		parameters.put("order_amount", amount);
+		parameters.put("order_add_option", add_option.toString());
+		parameters.put("order_amount", amount.toString());
 		parameters.put("order_date", orderDTO.getOrder_date());
-		parameters.put("ordermade_no", made_no);
+		parameters.put("ordermade_no", made_no.toString());
 		mapper.insertOrder(parameters);
-		for (Map<Integer, Integer> noStock : option) {
+
+		for (Map<String, Integer> noStock : option) {
 			mapper.updateOption_stock(noStock);
 		}
 	}
@@ -66,8 +74,8 @@ public class OrderService {
 		parameters.put("user_name", user_name);
 		parameters.put("user_call", user_call);
 		parameters.put("user_address", user_address);
-		// mapper.updateName(parameters);
-		// mapper.updateCall(parameters);
-		// mapper.updateAddress(parameters);
+		mapper.updateName(parameters);
+		mapper.updateCall(parameters);
+		mapper.updateAddress(parameters);
 	}
 }
--- src/main/webapp/WEB-INF/views/order/test.jsp
+++ src/main/webapp/WEB-INF/views/order/test.jsp
@@ -7,7 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="test" method="post">
+	<form action="p1" method="post">
 		<input type="text" name="user_name" id="user_name"> <input
 			type="text" name="user_call" id="user_call"> <input
 			type="text" name="user_address" id="user_address"> <input
