80558eb81be45739c7177a9b090b6231172f65cd
min <min@DESKTOP-ND42P04>
2019-08-21 13:07:43 +0900

    작가 페이지 만드는중

---
+++ src/main/java/com/bitcamp/DTO/artist/ArtistBoardDTO.java
@@ -0,0 +1,17 @@
+package com.bitcamp.DTO.artist;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter @Setter
+public class ArtistBoardDTO {
+	private int artist_no;
+	private int member_no;
+	private String artist_board_title;
+	private String artist_main_img;
+	private int artist_count;
+	private int artist_score;
+	private int artist_board_status;
+}
